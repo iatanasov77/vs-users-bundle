06cdff12a4b24c38b674e29cb9a4471e14b7aede
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-07-29 14:30:33 +0000

    Create a SuperAdminAccessTokenAuthenticator .

--- Security/SuperAdminAccessTokenAuthenticator.php
+++ Security/SuperAdminAccessTokenAuthenticator.php
@@ -1,8 +1,10 @@
 <?php namespace Vankosoft\UsersBundle\Security;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Http\Authenticator\AccessTokenAuthenticator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
 use Symfony\Component\Security\Http\AccessToken\QueryAccessTokenExtractor;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -11,7 +13,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Vankosoft\UsersBundle\Model\Interfaces\UserInterface;
 
-class SuperAdminAccessTokenAuthenticator extends AccessTokenAuthenticator
+class SuperAdminAccessTokenAuthenticator implements AuthenticatorInterface
 {
     public function supports( Request $request ): ?bool
     {
@@ -49,5 +51,20 @@ class SuperAdminAccessTokenAuthenticator extends AccessTokenAuthenticator
             public function getActivities(): Collection { return new ArrayCollection(); }
             public function getNotifications(): Collection { return new ArrayCollection(); }
         }));
+    }
+    
+    public function createToken( Passport $passport, string $firewallName ): TokenInterface
+    {
+        return new PostAuthenticationToken( $passport->getUser(), $firewallName, $passport->getUser()->getRoles() );
+    }
+    
+    public function onAuthenticationSuccess( Request $request, TokenInterface $token, string $firewallName ): ?Response
+    {
+        return null;
+    }
+    
+    public function onAuthenticationFailure( Request $request, AuthenticationException $exception ): ?Response
+    {
+        return null;
     }
 }
