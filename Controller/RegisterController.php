080bafa32d9af54adfbea04c40c05754d7067ee3
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-07-29 18:37:04 +0000

    Fix a Deprecation of VerifyEmail.

--- Controller/RegisterController.php
+++ Controller/RegisterController.php
@@ -176,7 +176,7 @@ class RegisterController extends AbstractController
         }
                 
         try {
-            $this->verifyEmailHelper->validateEmailConfirmation( $request->getUri(), $user->getId(), $user->getEmail() );
+            $this->verifyEmailHelper->validateEmailConfirmationFromRequest( $request->getUri(), $user->getId(), $user->getEmail() );
         } catch ( VerifyEmailExceptionInterface $e ) {
             $this->addFlash( 'verify_email_error', $e->getReason() );
             
