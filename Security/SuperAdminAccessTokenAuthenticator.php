bfd7cb9681cb647483a8abf3c73f5e8af95e8cfc
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-07-29 14:42:55 +0000

    Create a SuperAdminAccessTokenAuthenticator .

--- Security/SuperAdminAccessTokenAuthenticator.php
+++ Security/SuperAdminAccessTokenAuthenticator.php
@@ -22,20 +22,6 @@ class SuperAdminAccessTokenAuthenticator implements AuthenticatorInterface
     
     public function authenticate( Request $request ): Passport
     {
-        $apiToken = $request->headers->get('X-AUTH-TOKEN');
-        
-        if ( null === $apiToken ) {
-            // The token header was empty, authentication fails with HTTP Status
-            // Code 401 "Unauthorized"
-            //throw new CustomUserMessageAuthenticationException( 'No API token provided' );
-        }
-        
-        // Check token is correct
-        if ( $apiToken === $this->expectedApiToken ) {
-            // fail authentication with a custom error
-            //throw new CustomUserMessageAuthenticationException( 'Invalid API token' );
-        }
-        
         // Use anonymous class which implements UserInterface.
         return new SelfValidatingPassport( new UserBadge( $apiToken, fn() => new class implements UserInterface {
             public function getRoles(): array { return ['ROLE_SUPER_ADMIN']; }
