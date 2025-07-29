e6b09fab90765c62c5482561c4af9c802361dcee
Ivan I. Atanasov <i.atanasov77@gmail.com>
2025-07-29 14:37:13 +0000

    Create a SuperAdminAccessTokenAuthenticator .

--- Security/SuperAdminAccessTokenAuthenticator.php
+++ Security/SuperAdminAccessTokenAuthenticator.php
@@ -3,8 +3,8 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Http\Authenticator\AuthenticatorInterface;
-use Symfony\Component\Security\Http\AccessToken\QueryAccessTokenExtractor;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
